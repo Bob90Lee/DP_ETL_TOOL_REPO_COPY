63d825bf7c93f8186532c93585138e1a4e412043
Boris Ledecky <boris.ledecky@gmail.com>
2016-04-06 19:42:16 +0200

    fuck off monkey

---
+++ DP_ETL_TOOL/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DP_ETL_TOOL
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            MenuStrip menuBar = new MenuStrip();
+
+
+            menuBar.Items.Add("Exit", null, new EventHandler(MenuBarClickHandler));
+            menuBar.Items.Add("Fuck off");
+            menuBar.Items.Add("monkey");
+
+            this.Controls.Add(menuBar);
+        }
+
+        private void MenuBarClickHandler(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
--- DP_ETL_TOOL/Program.cs
+++ DP_ETL_TOOL/Program.cs
@@ -9,16 +9,26 @@ namespace DP_ETL_TOOL
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Boris Ledecky - Diplomova praca - ETL Tool
         /// </summary>
+        /// 
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Form main = new MainForm();
+            main.Text = "ETL Toolkit";
+
+            main.SetBounds(0, 0, 640, 480);
+            main.FormBorderStyle = FormBorderStyle.Sizable;
+
+            Application.Run(main);            
 
             System.Console.Out.WriteLine("Hello World!");
         }
+
+
     }
 }
